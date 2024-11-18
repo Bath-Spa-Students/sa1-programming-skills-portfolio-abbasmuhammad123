4538df7baa416e4b99d27c9d5669ad3f51e3515e
Muhammad Abbas <muhammadabbasghulamshabbir927@gmail.com>
2024-11-18 20:19:42 +0400

    Simple Sums

---
+++ Exercise02/Simple Sums.py
@@ -0,0 +1,27 @@
+'''Exercise 2: Simple Sums - 15 Marks
+In this exercise, you will create and work with integer variables, perform arithmetic operations, and print the result to the console.
+
+Steps:
+Declare a variable and initialize it with the integer value 8.
+Declare a second variable and initialize it with the integer value 10.
+Declare a third variable that stores the sum of first two numbers.
+Print the value of the sum to the console.'''
+
+# Step 1: Declare a variable and initialize it with the integer value 8
+num_1 = 8
+
+# Step 2: Declare a second variable and initialize it with the integer value 10
+num_2 = 10
+
+# Step 3: Declare a third variable and initialize it with the integer value 34
+num_3 = 34
+
+# Step 4: Declare a forth variable and initialize it with the integer value 56
+num_4 = 56
+
+# Define a variable to hold the sum of the first four numbers
+sum_of_numbers = num_1 + num_2 + num_3 + num_4 # Calculates the sum of the first four numbers
+
+# Step 6: it Prints the values of the sum to the console
+print("The sum of", num_1, num_2, num_3, "and", num_4, "is:", sum_of_numbers)
+
